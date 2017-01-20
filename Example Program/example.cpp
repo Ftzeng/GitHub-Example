3fbceefc0b391781c6827f18e63cdf82b1242f27
Ftzeng <ftzeng@stumail.jccc.edu>
2017-01-19 18:46:40 -0600

    FeiShian.cpp
    I just compile the code from the professor and type what it ask.

--- Example Program/example.cpp
+++ Example Program/example.cpp
@@ -4,20 +4,29 @@ using namespace std;
 
 int main()
 {
-    // Variable declarations
-    string name, color;
+	// Variable declarations
+	string name, color;
 
-    // Output and input
-    cout << "Hello, what is your name? ";
-    cin >> name;
+	// Output and input
+	cout << "Hello, what is your name? ";
+	cin >> name;
 
-    cout << "What is your favorite color? ";
-    cin >> color;
+	cout << "What is your favorite color? ";
+	cin >> color;
 
-    cout << endl;
-    cout << "So can I call you " << color << " " << name << "?" << endl;
+	cout << endl;
+	cout << "So can I call you " << color << " " << name << "?" << endl; 
 
-	// test 1 2 3
-
-    return 0;
+	return 0;
 }
+
+
+
+
+/*
+Hello, what is your name? Ben
+What is your favorite color? Yellow
+
+So can I call you Yellow Ben?
+Press any key to continue . . .
+*/
